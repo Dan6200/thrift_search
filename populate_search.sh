097c941217e82c6d5151323e3a8665eba3edfddf
Daniel Nyong <danielnyong66@gmail.com>
2023-09-08 18:22:47 +0100

    changed path from products.json to /opt/src/products

--- populate_search.sh
+++ populate_search.sh
@@ -3,4 +3,4 @@
 curl \
   -X POST 'https://thrift-search.onrender.com/indexes/products/documents?primaryKey=product_id' \
   -H 'Content-Type: application/json' \
-  --data-binary @products.json
+  --data-binary @/opt/src/products.json
